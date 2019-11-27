80c65dce4c64f6602a765e71259119cca0e5d90b
Gintare Vilkelyte <vilkelyte.gintare@gmail.com>
2019-11-27 11:07:01 +0100

    feat: adding getBranchPath method

---
+++ vars/getBranchPath.groovy
@@ -0,0 +1,15 @@
+def call(isTag) {
+  def branch_name = env.BRANCH_NAME
+  def job_path = $JENKINS_HOME/jobs/${getJobName()}
+  def branch_path = job_path + "/branches/"
+  if (isTag) {
+    def replaced_name = branch_name.replace(".", "-");
+    def tag_folder = sh (
+      script: "find $branch_path -name $replaced_name*",
+      returnStdout: true
+    ).trim()
+    branch_path = tag_folder;
+  }
+
+  isTag ? branch_path : "$branch_path/$branch_name"
+}
