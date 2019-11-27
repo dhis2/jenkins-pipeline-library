74ad792672f5ee96559acc7d7b6e792c083f1487
Gintare Vilkelyte <vilkelyte.gintare@gmail.com>
2019-11-27 11:13:54 +0100

    fix: find path

--- vars/getBranchPath.groovy
+++ vars/getBranchPath.groovy
@@ -1,7 +1,7 @@
 def call(isTag) {
   def branch_name = env.BRANCH_NAME
   def job_path = env.JENKINS_HOME + "/jobs/${getJobName()}"
-  def branch_path = job_path + "/branches/"
+  def branch_path = job_path + "/branches"
   if (isTag) {
     def replaced_name = branch_name.replace(".", "-");
     def tag_folder = sh (
