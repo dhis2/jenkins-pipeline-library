0edea61b39b9614c9bc29a31810435961c249b3c
Gintare Vilkelyte <vilkelyte.gintare@gmail.com>
2020-01-16 14:22:20 +0100

    fix: jsonSlurper

--- vars/awx.groovy
+++ vars/awx.groovy
@@ -12,10 +12,7 @@ def resetWar(credentials, host, name) {
 	def response = ["curl", "-u", credentials, "-k", "-X", "POST", "-H", "Content-Type: application/json", "-d", "${json}", "https://awx.dhis2.org/api/v2/job_templates/10/launch/"].execute().text
   echo "$response"
 
-  def job_id=sh(
-    script: "echo $response | python -c 'import sys, json; print json.load(sys.stdin)[\"job\"]' ",
-    returnStdout: true
-  )
+  def job_id = new JsonSlurper().parseText(response).get("job")
 
   def status="not_started"
 
@@ -24,10 +21,7 @@ def resetWar(credentials, host, name) {
     sleep(10) 
     def job_response=["curl", "-u", credentials, "-X", "GET", "-s", "https://awx.dhis2.org/api/v2/jobs/$job_id/"].execute().text
            
-    status=sh(
-      script: "echo $job_response | python -c 'import sys, json; print json.load(sys.stdin)[\"status\"]'",
-      returnStdout: true
-    )
+    status = new JsonSlurper().parseText(job_response).get("status")
 
     if(status=='failed') {
       System.exit(0)
