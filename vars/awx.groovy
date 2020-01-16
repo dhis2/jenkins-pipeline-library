f6483f083656f045f3f2e19956e548e91255f7e6
Gintare Vilkelyte <vilkelyte.gintare@gmail.com>
2020-01-16 13:49:42 +0100

    feat: adding awx var with resetWar method

---
+++ vars/awx.groovy
@@ -0,0 +1,27 @@
+def resetWar(credentials, host, name) {
+  def json = "{\"extra_vars\":{\"instance_host\":\"$host\",\"instance_name\": \"$name\",\"instance_action\": \"reset_war\"}}"
+	def response = ["curl", "-u", credentials, "-k", "-X", "POST", "-H", "Content-Type: application/json", "-d", "${json}", "https://awx.dhis2.org/api/v2/job_templates/10/launch/"].execute().text
+  echo "$response"
+
+  def job_id=sh(
+    script: "echo $response | python -c 'import sys, json; print json.load(sys.stdin)[\"job\"]' ",
+    returnStdout: true
+  )
+
+  def status="not_started"
+
+  while (status != "successful") {
+    echo "$status"
+    sleep(10) 
+    def job_response=["curl", "-u", "$credentials", "-X", "GET", "-s", "https://awx.dhis2.org/api/v2/jobs/$job_id/"].execute().text
+           
+    def status=sh(
+      script: "echo $job_response | python -c 'import sys, json; print json.load(sys.stdin)[\"status\"]'",
+      returnStdout: true
+    )
+
+    if(status=='failed') {
+      System.exit(0)
+    }
+  }
+}
