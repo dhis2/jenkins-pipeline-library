cc53c1d672cbdf585ad8b8261c00d2c41cccdded
Gintare Vilkelyte <vilkelyte.gintare@gmail.com>
2020-01-20 13:36:39 +0100

    feat: update awx instance
    * feat: adding awx var with resetWar method
    
    * fix: removes repeating var definition
    
    * fix: removes quotes in var
    
    * fix: beautifying json
    
    * fix: import statement
    
    * fix: json
    
    * temp: debug statement
    
    * fix: instance hosts should be a list
    
    * fix: quoting
    
    * fix: hosts array
    
    * fix: jsonSlurper
    
    * fix: shorten sleep
    
    * fix: exit
    
    * fix: return status
    
    * fix: testing status
    
    * fix: logging
    
    * fix: trying return 0
    
    * fix: trying assert

---
+++ vars/awx.groovy
@@ -0,0 +1,23 @@
+import groovy.json.*
+
+def resetWar(credentials, host, name) {
+  def json = JsonOutput.toJson(
+    [extra_vars: [
+      instance_host: [host], 
+      instance_name: name, 
+      instance_action: "reset_war"
+    ]]
+  )
+	def response = ["curl", "-u", credentials, "-k", "-X", "POST", "-H", "Content-Type: application/json", "-d", "${json}", "https://awx.dhis2.org/api/v2/job_templates/10/launch/"].execute().text
+  def job_id = new JsonSlurper().parseText(response).get("job")
+  def status="not_started"
+
+  while (status != "successful") {
+    echo "Ansible job status: $status"
+    sleep(2) 
+    def job_response=["curl", "-u", credentials, "-X", "GET", "-s", "https://awx.dhis2.org/api/v2/jobs/$job_id/"].execute().text
+           
+    status = new JsonSlurper().parseText(job_response).get("status")
+    assert status != "failed" : "Ansible job failed. Exiting.."
+  }
+}
