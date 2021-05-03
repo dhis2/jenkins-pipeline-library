3c0b0d55e26edc50cc8d47a30b65534a658836ed
Gintare Vilkelyte <vilkelyte.gintare@gmail.com>
2021-05-03 15:42:09 +0200

    adds d2

---
+++ vars/d2.groovy
@@ -0,0 +1,21 @@
+def startCluster( credentials, name, port ) {
+    sh "d2 cluster --channel dev --port ${port} up --update ${name}"
+    
+    wait("$credentials", "$port")
+
+}
+
+def startClusterWithSL( credentials, name, port ) {
+    sh "d2 cluster --channel dev --port ${port} up --update ${name} --seed"
+    wait("$credentials", "$port")
+}
+
+def wait(credentials, port) {
+   def response = "not_started"
+    
+    while (response != "200") {
+        echo "status: $response"
+        sleep(5) 
+        response = ["curl", "-k", "-X", "GET", "-w", "%{http_code}", "--silent", "-o /dev/null", "-u", "${credentials}", "http://localhost:${port}/api/system/info"].execute().text
+    }
+}
